ee7d65d7942f9fd658c8931f535721795511d7c3
SD91PL <sd91pl@gmail.com>
2024-09-09 11:49:22 +0200

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import Footer from "./components/Footer.jsx"
+import Gallery from "./components/Gallery.jsx"
+import Headers from "./components/Headers.jsx"
+import Hero from "./components/Hero.jsx"
+import Nav from "./components/Nav.jsx"
+import Seo from "./components/Seo.jsx"
 
 function App() {
   return (
     <>
-      
+      <main className="bg-[#f7f7f7] min-h-screen min-w-screen">
+
+      </main>
     </>
   )
 }
