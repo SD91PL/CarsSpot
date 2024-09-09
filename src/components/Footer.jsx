aad9c157d5f7b4887cc4337ab8c74619d076ed60
SD91PL <sd91pl@gmail.com>
2024-09-09 13:02:46 +0200

    fix: footer top border

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
 	return (
-		<footer className='bg-[#282828] h-[72px] min-w-screen border-t border-solid border-white'>
+		<footer className='bg-[#282828] min-w-screen border-t border-solid border-white'>
 			<div className='mx-4'>
 				<div className='container'>
-					<div className='flex justify-between items-center h-[72px] text-white text-base'>
+					<div className='flex justify-between items-center h-[72px] text-white text-base '>
 						<a href='#'>
 							<p className='font-bold'>Cars Spot</p>
 						</a>
