181770265abf499b439b36e66782a1a5bab1387f
SD91PL <sd91pl@gmail.com>
2024-09-11 13:35:01 +0200

    added generating alt attribute values for images based on file names

--- src/components/GallerySlider.jsx
+++ src/components/GallerySlider.jsx
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react'
-// import { useRef } from 'react'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
 
 export default function GallerySlider() {
 	const [images, setImages] = useState([])
+
 	useEffect(() => {
 		const importImages = async () => {
+			// Import all .webp images
 			const images = import.meta.glob('../assets/img/gallery/*.webp')
+			// Load the images
 			const imagePromises = Object.keys(images).map(async path => {
 				const image = await images[path]()
-				return image.default
+				// Return an object with the image path and file name (without extension)
+				return { path: image.default, fileName: path.split('/').pop().replace('.webp', '') }
 			})
 			const loadedImages = await Promise.all(imagePromises)
 			setImages(loadedImages)
@@ -19,8 +22,6 @@ export default function GallerySlider() {
 
 		importImages()
 	}, [])
-
-	// const slider = useRef(null)
 
 	const settings = {
 		dots: true,
@@ -30,25 +31,30 @@ export default function GallerySlider() {
 		slidesToScroll: 1,
 	}
 
+	// Function to generate dynamic alt text based on the file name
+	const generateAltText = fileName => {
+		// Remove the numeric prefix (e.g., "01-", "02-") from the file name
+		const fileNameParts = fileName.split('-').slice(1)
+		// Join the remaining parts of the name and add the "samochód" (car) prefix
+		return `samochód-${fileNameParts.join('-')}`
+	}
+
 	return (
-		<>
-			<div>
-				{/* <button onClick={() => slider?.current.slickPrev()}>Wstecz</button> */}
-				<Slider
-					className='mb-20'
-					// ref={slider}
-					{...settings}>
-					{images.map((image, index) => (
-						<div key={index}>
-							<img	
-								src={image}
-								alt={`gallery-img-${index}`}
-							/>
-						</div>
-					))}
-				</Slider>
-				{/* <button onClick={() => slider?.current.slickNext()}>Dalej</button> */}
-			</div>
-		</>
+		<div>
+			<Slider
+				className='mb-20 '
+				{...settings}>
+				{images.map((image, index) => (
+					<div
+						key={index}
+						className='outline-none'>
+						<img
+							src={image.path}
+							alt={generateAltText(image.fileName)} // Generate dynamic alt text
+						/>
+					</div>
+				))}
+			</Slider>
+		</div>
 	)
 }
