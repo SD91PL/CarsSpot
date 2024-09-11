5e13f6a1437bdf169ac6ead75724e4b38571947d
SD91PL <sd91pl@gmail.com>
2024-09-11 17:09:04 +0200

    fix: filter buttons styling

--- src/components/GallerySlider.jsx
+++ src/components/GallerySlider.jsx
@@ -48,8 +48,8 @@ export default function GallerySlider() {
 	})
 
 	// Filter buttons styling
-	const btnStyle = 'transition-colors underline underline-offset-[.35rem] decoration-transparent'
-	const highlighted = 'text-[#0147FF] decoration-inherit'
+	const btnStyle = 'transition-colors underline underline-offset-[.35rem] decoration-[#0000]'
+	const highlighted = 'text-[#0147FF] decoration-current'
 
 	return (
 		<div>
