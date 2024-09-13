1a07d5c8bda76abf63cb587d1cdf066aeafd6c63
SD91PL <sd91pl@gmail.com>
2024-09-13 13:59:37 +0200

    work on RWD of Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,27 @@
-// import HeroBg from '../assets/img/Element.webp'
 import HeroBg from '../assets/img/svg/Element.svg'
+// import HeroBg from '../assets/img/Element.webp'
 import HeroImg from '../assets/img/Hero.webp'
 
 export default function Hero() {
 	return (
 		<>
-			<section id='hero'>
+			<section
+				id='hero'
+				className='sm:h-full'>
 				<div className='mx-4'>
-					<div className='container relative min-h-[700px] md:min-h-[805px] lg:min-h-[905px] xl:max-w-[1320px]'>
+					<div className='container relative min-h-[720px] md:min-h-[805px] lg:min-h-[905px] xl:max-w-[1320px]'>
 						<div className='container '>
 							<div className='hero-front absolute flex flex-col justify-center py-40 md:py-0 md:pb-40 h-full xl:min-h-[905px] z-20'>
-								<div className='flex flex-col gap-12 xl:w-3/4'>
+								<div className='flex flex-col gap-12 lg:w-3/4'>
 									<div className='hero-header flex flex-col gap-8'>
-										<h1 className='bebas-neue text-[4.75rem] leading-[1.1]'>Sprzedajemy samochody z Europy</h1>
+										<h1 className='bebas-neue text-[4.75rem] leading-[1.1]'>Sprzedajemy samochody z&nbsp;Europy</h1>
 										<p className='xl:w-1/2'>Kup wyjatkowy pojazd, aby każda podróż była wyjątkowym przeżyciem</p>
 									</div>
-									<div className='hero-cta flex flex-wrap gap-6 text-[.9375rem] roboto-flex'>
+									<div className='hero-cta flex flex-wrap gap-2 gap-y-4 sm:gap-6 text-[.9375rem] roboto-flex font-semibold'>
 										<a
 											href='#gallery'
 											rel='noopener'
-											className='px-6 py-3 rounded-lg bg-[#0147FF] text-[#F7F7F7]'>
+											className='px-6 py-3 rounded-lg bg-[#0147FF] border-[#0147FF] border-[1px] text-[#F7F7F7]'>
 											Zobacz zdjęcia
 										</a>
 										<a
@@ -33,12 +35,13 @@ export default function Hero() {
 							</div>
 						</div>
 						{/* Background*/}
-						<div className='hero-back z-0'>
-							<div className='flex justify-end'>
+						<div className='hero-back z-0 opacity-30 md:opacity-50 xl:opacity-100'>
+							<div className='relative flex justify-end'>
 								<img
 									src={HeroBg}
 									alt=''
 								/>
+								<div className='absolute  h-full w-full bg-gradient-to-b from-[#0000] sm:via-90% via-[#0000] to-[#F7F7F7]'></div>
 							</div>
 						</div>
 					</div>
