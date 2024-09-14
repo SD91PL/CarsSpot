16208e0f621c82f016c5a8659f2a84ef02bc17db
SD91PL <sd91pl@gmail.com>
2024-09-14 20:22:10 +0200

    restructuring the code of the entire site

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import Footer from "./components/Footer.jsx"
-import Gallery from "./components/Gallery.jsx"
+import Footer from './components/Footer.jsx'
+import Gallery from './components/Gallery.jsx'
 // import Headers from "./components/Headers.jsx" // in Gallery section
-import Hero from "./components/Hero.jsx"
-import Nav from "./components/Nav.jsx"
-import Seo from "./components/Seo.jsx"
+import Hero from './components/Hero.jsx'
+import Nav from './components/Nav.jsx'
+import Seo from './components/Seo.jsx'
 
 function App() {
-  return (
-    <div className="roboto-condensed">
-      <Nav/>
-      <Hero/>
-      <Gallery />
-      <Seo/>
-      <Footer/>
-    </div>
-  )
+	return (
+		<>
+			<Nav />
+			<main>
+				<Hero />
+				<Gallery />
+				<Seo />
+			</main>
+			<Footer />
+		</>
+	)
 }
 
 export default App
