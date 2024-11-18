fb9a20f4a83179ada8101a3b3a60f05ec21cfc9f
SD91PL <sd91pl@gmail.com>
2024-11-18 11:37:08 +0100

    Revert "added focus-in animations"
    This reverts commit 0b1a67f5bcb198fcefbed36f24ed164eef85caef.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ export default function Gallery() {
 	return (
 		<section
 			id='gallery'
-			className='mx-4 focus-in animation-delay-900'>
+			className='mx-4'>
 			<div className='container xl:max-w-[1328px] overflow-hidden'>
 				<Headers
 					top='Prezentacja firmy'
