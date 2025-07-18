b416f60617a8fd90c852983f630ac031c20a39a7
SD91PL <sd91pl@gmail.com>
2025-07-18 10:00:25 +0200

    fixed gallery section margins

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ export default function Gallery() {
 	return (
 		<section
 			id='gallery'
-			className='mx-4'>
+			className='mx-4 xl:mx-0'>
 			<div className='container bg-[#f7f7f7] xl:max-w-[1440px]'>
 				<div className='container xl:max-w-[1328px] overflow-hidden'>
 					<Headers
